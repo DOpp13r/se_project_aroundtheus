1d057c65ea88699b3261bb5b9df75771a2b884a4
Nathan Do <nathando920@gmail.com>
2024-06-26 21:31:46 -0400

    Feat: validate.js + CSS properties

---
+++ scripts/validate.js
@@ -0,0 +1,14 @@
+// function showInputError(formElement, inputElement, {inputErrorClass, errorClass}) {
+
+// }
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
